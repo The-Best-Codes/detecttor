84275add0491fe943aabc3ee0e2a3d3df3577e1f
The-Best-Codes <bestcodes.official@gmail.com>
2024-10-03 09:35:30 -0500

    Update test

--- test/index.mjs
+++ test/index.mjs
@@ -1,6 +1,14 @@
-import { amIUsingTor } from "../dist/esm/index.js";
+import isIpTor, { amIUsingTor } from "../dist/esm/index.js";
 
 amIUsingTor()
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
+
+isIpTor("89.0.142.86")
     .then((result) => {
         console.log(result);
     })
