d69d3dba14846da63118f6431890423ed5a8834b
The-Best-Codes <bestcodes.official@gmail.com>
2025-05-15 10:56:53 -0500

    fmt tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
-import { fileURLToPath } from "url";
 import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
